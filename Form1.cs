6cbc4f3117ddcac424aacb4fe261062895574e44
Markus Wolf <mobiletoolbox0@gmail.com>
2025-11-17 10:30:22 +0100

    Projektdateien hinzufügen.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BFW_Project_011._2_G61_SQL_Bibliotheksverwaltung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
